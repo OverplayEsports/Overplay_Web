f490914063d48b44ee33bb1819e194231ffd026f
Pamache <pamacheyt@gmail.com>
2026-09-22 21:36:10 -0300

    feat: use Cloudflare R2 CDN URLs for rank badges

--- src/types/tournament.ts
+++ src/types/tournament.ts
@@ -18,16 +18,18 @@ export interface RankOption {
   image: string;
 }
 
+const R2_RANKS_CDN = "https://pub-def6d9ceb4ef4e8f84ee8a391d2b0b27.r2.dev/rangos";
+
 export const OVERWATCH_RANKS: RankOption[] = [
-  { id: "Bronce", name: "Bronce", image: "/images/Rangos/Bronce.png" },
-  { id: "Plata", name: "Plata", image: "/images/Rangos/Plata.png" },
-  { id: "Oro", name: "Oro", image: "/images/Rangos/Oro.png" },
-  { id: "Platino", name: "Platino", image: "/images/Rangos/Platino.png" },
-  { id: "Esmeralda", name: "Esmeralda", image: "/images/Rangos/Esmeralda.png" },
-  { id: "Diamante", name: "Diamante", image: "/images/Rangos/Diamante.png" },
-  { id: "Master", name: "Master", image: "/images/Rangos/Master.png" },
-  { id: "GrandMaster", name: "GrandMaster", image: "/images/Rangos/GrandMaster.png" },
-  { id: "Champeon", name: "Campeón", image: "/images/Rangos/Champeon.png" },
+  { id: "Bronce", name: "Bronce", image: `${R2_RANKS_CDN}/Bronce.png` },
+  { id: "Plata", name: "Plata", image: `${R2_RANKS_CDN}/Plata.png` },
+  { id: "Oro", name: "Oro", image: `${R2_RANKS_CDN}/Oro.png` },
+  { id: "Platino", name: "Platino", image: `${R2_RANKS_CDN}/Platino.png` },
+  { id: "Esmeralda", name: "Esmeralda", image: `${R2_RANKS_CDN}/Esmeralda.png` },
+  { id: "Diamante", name: "Diamante", image: `${R2_RANKS_CDN}/Diamante.png` },
+  { id: "Master", name: "Master", image: `${R2_RANKS_CDN}/Master.png` },
+  { id: "GrandMaster", name: "GrandMaster", image: `${R2_RANKS_CDN}/GrandMaster.png` },
+  { id: "Champeon", name: "Campeón", image: `${R2_RANKS_CDN}/Champeon.png` },
   { id: "Sin Rango", name: "Sin Rango / Unranked", image: "" },
 ];
 
